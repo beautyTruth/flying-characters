f1e239d280edd0e49ad23331825070762c5df5ba
Phil <happy.melodist@gmail.com>
2022-02-22 00:23:53 -0600

    pursue course through the creation of the enemy class at 9:20 of the video

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const canvasEl = document.querySelector("canvas");
 const ctx = canvasEl.getContext("2d");
 CANVAS_WIDTH = canvasEl.width = 500;
-CANVAS_HEIGHT = canvasEl.height = 100;
+CANVAS_HEIGHT = canvasEl.height = 1000;
+
+/*
+enemy1 = {
+  x: 0,
+  y: 0,
+  width: 200,
+  height: 200,
+};
+*/
+
+class Enemy {
+  constructor() {
+    this.x = 10;
+    this.y = 50;
+    this.width = 100;
+    this.height = 100;
+  }
+}
+
+const enemy1 = new Enemy();
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.x++;
+  enemy1.y++;
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  requestAnimationFrame(animate);
+}
+
+animate();
